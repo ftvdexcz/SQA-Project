5d0b8d6c4eb27f994b1d6405910cd5afafc895f6
ftvdexcz <ftvdexc95@gmail.com>
2023-04-11 00:51:59 +0700

    first-commit

---
+++ WaterBilling/src/test/java/com/sqa/g06/n03/WaterBilling/WaterBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqa.g06.n03.WaterBilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

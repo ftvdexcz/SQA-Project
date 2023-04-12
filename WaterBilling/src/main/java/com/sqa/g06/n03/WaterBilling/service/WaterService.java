0b97fe4033c29219c05d5556f40e66bfb84c6a09
ftvdexcz <ftvdexc95@gmail.com>
2023-04-12 15:43:07 +0700

    add function for calculator bill

--- WaterBilling/src/main/java/com/sqa/g06/n03/WaterBilling/service/WaterService.java
+++ WaterBilling/src/main/java/com/sqa/g06/n03/WaterBilling/service/WaterService.java
@@ -2,7 +2,7 @@ package com.sqa.g06.n03.WaterBilling.service;
 
 public interface WaterService {
     public double calcAmount(int meterConsume);
-    public double calcTax(double amount, int taxRate);
+    public double calcTax(double amount);
 
-    public double calcEnvironmentFee(double amount, int environmentRate);
+    public double calcEnvironmentFee(double amount);
 }
